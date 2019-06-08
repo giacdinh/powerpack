e205c5200f83ad0dc4f90d45b3b8af4e992a8e1a
ghecu <ghecu@hotmail.com>
2019-06-07 20:14:19 -0400

    Add main action inside of ctrl

--- ctrl/controller.c
+++ ctrl/controller.c
@@ -6,12 +6,13 @@
 #include <pthread.h>
 
 void *ctrl_dog_bark_task();
+void *ctrl_worker_task();
 void ctrl_msg_handler(CTRL_MSG_T *Incoming);
 
 int ctrl_main_task()
 {
 	int result = -1;
-	pthread_t ctrl_wd_id = -1;
+	pthread_t ctrl_wd_id = -1, ctrl_main_id = -1;
 
     CTRL_MSG_T ctrl_msg;
 	logging(DBG_INFO, "Create controller message\n");
@@ -23,7 +24,7 @@ int ctrl_main_task()
 		return 0;
 	}	
 
-	/* Create thread for DBASE WD response */
+	/* Create thread for WD response */
 	if(ctrl_wd_id == -1)
 	{
 		result = pthread_create(&ctrl_wd_id, NULL, ctrl_dog_bark_task, NULL);
@@ -31,6 +32,16 @@ int ctrl_main_task()
 	        logging(DBG_DETAILED, "Starting controller dog bark thread.\n");
         else
             logging(DBG_ERROR, "CTRL WD thread launch failed\n");
+	}
+
+	/* Create thread for CTRL main task */
+	if(ctrl_main_id == -1)
+	{
+		result = pthread_create(&ctrl_main_id, NULL, ctrl_worker_task, NULL);
+        if(result == 0)
+	        logging(DBG_DETAILED, "Starting controller main task thread.\n");
+        else
+            logging(DBG_ERROR, "CTRL main task thread launch failed\n");
 	}
 
 	while(1) {
@@ -45,6 +56,35 @@ void *ctrl_dog_bark_task()
     while(1) {
         send_dog_bark(CTRL_MODULE_ID);
         sleep(1);
+    }
+}
+
+void *ctrl_worker_task()
+{
+	logging(1,"%s: Entering ...\n", __FUNCTION__);
+    while(1) 
+	{
+		logging(1, "%s: Execute repeat task from control\n", __FUNCTION__);
+		// start cellular modem connection
+		// system("sudo hologram network connect");
+		// sleep(2);
+		
+		// check for connectivity confirm 
+		// for(;;)
+		// {
+		//		struct hostent *hostinfo;
+		//		hostinfo = gethostbyname(BACSON_HOST_NAME);
+		//		if(hostinfo == NULL) // Not connected???
+		//			break;
+		// }
+
+		// send infor to server
+		// get_cellular_coordinate();
+		// build_data_send_script();
+		// execute script
+
+		
+        sleep(10);
     }
 }
 
