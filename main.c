75591dda5c5c3c653c25a473afa8c62cee855628
ghecu <ghecu@hotmail.com>
2019-11-07 11:14:27 -0500

    Release 1.0.3.4

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 #include <sys/prctl.h>
 #include "build.h"
 
-#define APP_VERSION "1.0.3.3"
+#define APP_VERSION "1.0.3.4"
 extern void *wdog_main_task();
 extern void *remotem_main_task();
 extern void *ctrl_main_task();
