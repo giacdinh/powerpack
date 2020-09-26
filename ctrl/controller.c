c6532b335e543347a03331adc2f9605b8a463715
ghecu <ghecu@hotmail.com>
2020-09-26 10:48:20 -0400

    Add copyright header to all source code files

--- ctrl/controller.c
+++ ctrl/controller.c
@@ -1,3 +1,22 @@
+/*************************************************************************
+ * 
+ * Bac Son Technologies  
+ * __________________
+ * 
+ *  [2019] Bac Son Technologies LLC 
+ *  All Rights Reserved.
+ * 
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Bac Son Technologies LLC and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Bac Son Technologies LLC 
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Bac Son Technologies LLC.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
